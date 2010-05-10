39c1f76feaa2b9296270174a8a5c3053d152a266
Edwin Knuth <eknuth@app.photomapapp.com>
2010-05-10 19:49:56 +0000

    fixed preload images for jqtouch

--- static/js/mobile.js
+++ static/js/mobile.js
@@ -2,10 +2,13 @@ $.jQTouch({
 	icon: 'jqtouch.png',
 	    statusBar: 'black-translucent',
 	    preloadImages: [
+
+			    '/static/jqtouch/themes/img/toolbar.png',
+			    '/static/jqtouch/themes/img/chevron.png',
 			    '/static/jqtouch/img/chevron.png',
-			    '/static/jqtouch/img/back_button_clicked.png',
-			    '/static/jqtouch/img/button_clicked.png'
-			    ]
+			    '/static/jqtouch/img/back_button_clicked.png', 
+			    '/static/jqtouch/img/button_clicked.png',
+		    ]
 	    });
 var neighborhood = "unknown";
 if (neighborhood == "unknown") {
